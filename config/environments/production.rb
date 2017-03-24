6220fd2e9c275b8df61b88bf6011e60492fb1641
lpbauer <lukephilip@gmail.com>
2017-03-24 15:05:23 -0700

    changed keys for env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('apartmentapp032417'),
-    access_key_id: ENV.fetch('AKIAI4QKXHSN2QJRTWOA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
   # Code is not reloaded between requests.
