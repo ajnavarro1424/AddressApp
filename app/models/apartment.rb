d32f085b2f04fb254d4b8c21b63fd323f23eec9a
Alex Navarro <ajnavarro1424@gmail.com>
2017-03-30 11:55:48 -0700

    Added S3 credentials into the Apartment model

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,9 +3,17 @@ class Apartment < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
   has_attached_file :image, styles: { small: "75x75", med: "300x300", large: "600x600" }
-  validates_attachment :image, presence: true,
+  validates_attachment :image,
+    presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-    size: { in: 0..10.megabytes }, :s3_host_name => 's3-us-west-1.amazonaws.com'
+    size: { in: 0..10.megabytes },
+    :storage => :s3,
+    :s3_host_name => 's3-us-west-1.amazonaws.com'
+    :s3_credentials => {
+        :bucket => ENV['apartmentapp032417'],
+        :access_key_id => ENV['AKIAI4QKXHSN2QJRTWOA'],
+        :secret_access_key => ENV['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']
+
   resourcify
 
   def full_address
